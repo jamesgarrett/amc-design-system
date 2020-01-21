aed7ca3fd874bc704a5d39ee9e168b5b4f9828f6
jamesgarrett <jgsibinga@gmail.com>
2020-01-21 17:57:02 -0500

    Updates

---
+++ precache-manifest.8090cc03c9136ffc95e363423894782f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bc1af50a666bc67d28e9c324a9fa15",
+    "url": "/design-system-poc/index.html"
+  },
+  {
+    "revision": "35eb70699b1dbcb064a2",
+    "url": "/design-system-poc/static/css/main.0aec31d6.chunk.css"
+  },
+  {
+    "revision": "e2651f5da4c93cb5d48f",
+    "url": "/design-system-poc/static/js/2.0d205740.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/design-system-poc/static/js/2.0d205740.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35eb70699b1dbcb064a2",
+    "url": "/design-system-poc/static/js/main.086b72e5.chunk.js"
+  },
+  {
+    "revision": "d51513f546a28dd75f8e",
+    "url": "/design-system-poc/static/js/runtime-main.2dd3859b.js"
+  },
+  {
+    "revision": "15d644c82265b3b784a8e6a34a887824",
+    "url": "/design-system-poc/static/media/logo512-dark.15d644c8.png"
+  }
+]);
