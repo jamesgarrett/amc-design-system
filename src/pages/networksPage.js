980a4fdb2041e39bef3e0c3a2fafd85180fdb815
jamesgarrett <jgsibinga@gmail.com>
2020-03-12 23:32:57 -0400

    fix layout spacing on type, add content to intro from guidelines

--- src/pages/networksPage.js
+++ src/pages/networksPage.js
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom';
 const NetworksPage = () => {
 		const alt_text = ''
 return (
-	  <div className="m-x">
-	    <section className="intro m-b tl container-fixed p-y">
+	  <div className="intro p-x">
+	    <section className="m-b tl container-fixed p-y">
 	      <h1 className='headline-1 m-y'>Typography</h1>
 	      <h4 className='headline-4 m-y col-8'>About the System</h4>
 	      <p className='body-lg description m-y col-8'>The goal of the type system is to define a repeatable set of elements that can be deployed rapidly with consistent results. We use a core set of styles, and a fixed, unit independent sizing scale to generate consistent, readable type pairings.</p>
