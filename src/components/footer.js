f7f67600ef3d07666817a80c8ba89c1e61b26d50
jamesgarrett <jgsibinga@gmail.com>
2020-03-23 14:50:34 -0400

    Make footer year dynamic

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
+const today = new Date();
+
+
 class Footer extends React.Component{
 	render(){
 		return(
 			<footer className=''>
 				<div className='subtitle'>The digital assets and code included in these guidelines may only be used with the permission of, and for the benefit of, the AMC Networks Inc.</div>
-				<div className='subtitle'>©AMC Networks 2019</div>
+				<div className='subtitle'>©AMC Networks {today.getFullYear()}</div>
 			</footer>
 		)
 	}
-	
+
 }
 
 export default Footer;
