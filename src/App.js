54217e6116d69c2fdb84c27998ba6eb20c7b57ba
jamesgarrett <jgsibinga@gmail.com>
2020-04-13 09:59:53 -0400

    re-add the basename to router

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@
   import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
   const Main = () => (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/amc-design-system/">
       <Navigation />
       <Switch>
         <Route exact path='/' component={About}></Route>
