71fa2870905739393de1f7cb112cbb5f99f9428b
jamesgarrett <jgsibinga@gmail.com>
2020-04-14 17:45:42 -0400

    componentize our getbrand function

---
+++ src/components/getbrand.js
@@ -0,0 +1,21 @@
+import brands from  '../data/brands.js';
+
+function getBrand(network){
+	if (network === 'sundance'){
+		let brand = brands.sundance
+		return brand }
+	if (network === 'ifc'){
+		let brand = brands.ifc
+		return brand }
+	if (network === 'amc'){
+		let brand = brands.amc
+		return brand }
+	if (network === 'wetv'){
+		let brand = brands.wetv
+		return brand }
+	if (network === 'bbca'){
+		let brand = brands.bbca
+		return brand }
+}
+
+export default getBrand
