3d020929a1ca8601e4ea5f10b36478870380261f
jamesgarrett <jgsibinga@gmail.com>
2020-04-14 17:50:26 -0400

    Mega rewrite of resource detail makes resources page read data from JSON files using using network independent components

--- src/pages/resourceDetail.js
+++ src/pages/resourceDetail.js
@@ -1,37 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ResourceNav from '../components/resourcenav';
-import AmcResources from '../resources/amcResources';
-import SundanceResources from '../resources/sundanceResources';
-import IfcResources from '../resources/ifcResources';
-import BbcaResources from '../resources/bbcaResources';
+import { Link } from 'react-router-dom';
+import Colors from '../components/colors.js'
+import Logos from '../components/logos.js'
+import Fonts from '../components/fonts.js'
+import Placeholders from '../components/placeholders.js'
+import Icons from '../components/icons.js'
 
 const ResourceDetail = ({ match, location }) => {
 	const { params: { network } } = match;
-	var resourcePage;
-
-	if ( network === 'amc'){
-	  resourcePage = <AmcResources />;
-	} if ( network === 'sundance') {
-	  resourcePage = <SundanceResources />;
-  } if ( network === 'ifc') {
-	  resourcePage = <IfcResources />;
-	} if ( network === 'bbca') {
-	  resourcePage = <BbcaResources />;
-	}
-
-	var net = network;
 
 	return (
-	<div className={net}>
-	  <div className="intro p-x">
-      <section className="m-b tl container-fixed p-y">
-        <h1 className='headline-1 m-y'>{net} Resources</h1>
-        <h4 className='headline-4 m-y col-8'>About the System</h4>
-        <p className='body-lg description m-y col-8'>Our build system relies on a structure of shared and unique resources that both keep our network apps consistent, while accentuating the unique character that differentiate them.</p>
-      </section>
-	  </div>
-				<ResourceNav/>
-	      {resourcePage}
+	<div className={network}>
+		<div className="m-x">
+			<ResourceNav  />
+			<Logos network={network} />
+			<Colors network={network} />
+			<Fonts network={network} />
+			<Placeholders network={network} />
+			<Icons network={network} />
+		</div>
 	</div>
 	);
 };
